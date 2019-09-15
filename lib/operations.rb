5b0f4f115aaee117435638dda202773382ee1b7d
Kristan Johns <kristandjohns@gmail.com>
2019-09-15 17:56:04 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
-puts "Going below 40 on a freeway is unsafe!"
+if "Going below 40 on a freeway is unsafe!"
 end
 
 
 
 def not_safe?(speed)
-puts "Going above 70 on a freeway is not_safe!"
+if "Going above 70 on a freeway is not_safe!"
 end
 	
 
